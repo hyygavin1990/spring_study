fdf29ef6e66de61037e3bdd7681949c18d2c3b22
hyygavin <hyygavin1990@163.com>
2018-11-14 15:03:13 +0800

    kafka group interface

--- tomcat-v1/src/main/java/cn/datawin/TomcatV1Application.java
+++ tomcat-v1/src/main/java/cn/datawin/TomcatV1Application.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class TomcatV1Application {
 
 //	@Value("${name}")
