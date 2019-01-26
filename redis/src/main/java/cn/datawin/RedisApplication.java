baea215b1aa71fc87e9be39ade7312ff7387d38b
hyygavin <hyygavin1990@163.com>
2019-01-26 10:54:42 +0800

    kafka group interface

---
+++ redis/src/main/java/cn/datawin/RedisApplication.java
@@ -0,0 +1,30 @@
+package cn.datawin;
+
+import cn.datawin.redis.RedisService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@SpringBootApplication
+@RestController
+public class RedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+
+	@Resource
+	private RedisService redisService;
+
+	@RequestMapping(value = "/inc", method = RequestMethod.GET)
+	public String inc(HttpServletRequest request) {
+		return redisService.inc("test",1)+"";
+	}
+
+}
+
